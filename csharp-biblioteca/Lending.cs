61af976c63bdf9e60e08e42dbad718785fabae5f
AlessandroDiNardo <alexdna003@gmail.com>
2023-04-11 15:52:05 +0200

    fix

--- csharp-biblioteca/Lending.cs
+++ csharp-biblioteca/Lending.cs
@@ -8,5 +8,17 @@ namespace csharp_biblioteca
 {
     internal class Lending
     {
+        public User User { get; }
+        public string PeriodFrom { get; }
+        public string PeriodTo { get; set; }
+
+        public  Document Document { get; set; }
+        public Lending( string periodfrom, string periodto, Document document, User user) 
+        {
+            PeriodFrom = periodfrom;
+            PeriodTo = periodto;
+            Document = document;
+            User = user;
+        }
     }
 }
